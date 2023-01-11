1f0607f0e1db099736e872f54a6afaa52474944f
Sebastian <sebastian.sopola@edu.turkuamk.fi>
2023-01-11 09:53:56 +0200

    This is my first commit

---
+++ Exercise1/HelloWorld.c
@@ -0,0 +1,18 @@
+
+/*
+File:			HelloWorld.c
+Author: 		Sebastian Sopola
+Discription: 		Printing to screen
+*/
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello World\n");
+	
+	return 0;
+
+
+
+
+}
